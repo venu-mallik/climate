49705d061596ce050f049841528e085cb20159d8
vrworkers <venu.mallik@gmail.com>
2023-08-10 13:17:43 +0530

    Add fixed columns in antd tab

--- src/components/magnetic.js
+++ src/components/magnetic.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Statistic, Typography, Space, Card, Table, Select, Button } from "antd";
 import vegaEmbed from 'vega-embed';
-import { model} from 'geomagnetism';
+import { model } from 'geomagnetism';
 
 var map;
 
@@ -44,7 +44,7 @@ function runPlot(from, data, type) {
                     color: 'red',
                     fillColor: '#f03',
                     fillOpacity: 1 / (b.population),
-                    radius:  b.f ? b.f * 0.0001 : 0.1
+                    radius: b.f ? b.f * 0.0001 : 0.1
                 }).addTo(map).bindPopup(
                     `${b.name}-${Number(b.population).toFixed(0)}-${Number(b.f).toFixed(0)} nT`).openPopup();
         }
@@ -73,7 +73,7 @@ function vegaGeo(data) {
                 "encoding": {
                     "longitude": { "field": "lon", "type": "quantitative" },
                     "latitude": { "field": "lat", "type": "quantitative" },
-                    "color": {"field": "f", "bin": {"binned": true, "maxbins": 10 }},
+                    "color": { "field": "f", "bin": { "binned": true, "maxbins": 10 } },
                     "tooltip": [{ "field": "name" }, { "field": "population", "type": "quantitative" }, { "field": "f", "type": "quantitative" }]
                 },
                 "config": {
@@ -107,8 +107,8 @@ function vegaGeo(data) {
                 "encoding": {
                     "longitude": { "field": "lon", "type": "quantitative" },
                     "latitude": { "field": "lat", "type": "quantitative" },
-                    "size": {"value": 5},
-                    "color": {"field": "elevation", "type" : "ordinal"},
+                    "size": { "value": 5 },
+                    "color": { "field": "elevation", "type": "ordinal" },
                     "tooltip": [{ "field": "name" }, { "field": "elevation", "type": "quantitative" }]
                 },
                 "config": {
@@ -132,20 +132,20 @@ function vegaGeo(data) {
 export const MagneticComponent = (props) => {
 
     const [pop, setPop] = useState(0);
-    const [data,setData] = useState([])
+    const [data, setData] = useState([])
     const [selectedCities, setSelectedCities] = useState([]);
 
     useEffect(() => {
 
         let cities = props.data;
-        if (window && cities.length > 0 ) {
+        if (window && cities.length > 0) {
             let p = []
-            cities.map((r,i)=>{
-                let m = model(new Date()).point([r.lat,r.lon]);
-                p.push( { ...r ,  ...m} )
+            cities.map((r, i) => {
+                let m = model(new Date()).point([r.lat, r.lon]);
+                p.push({ ...r, ...m })
             })
             setData(p);
-            cities = p ;
+            cities = p;
             vegaGeo(cities)
             runPlot(cities[0], cities, 'bubble');
         }
@@ -168,37 +168,43 @@ export const MagneticComponent = (props) => {
         <>
 
             <Card title={props.country}   >
-            <Space direction="horizontal" >
-                <Statistic value={pop} title="Population"></Statistic>
-                <Statistic value={"length" in props.data ? props.data.length : 0} title="Areas" ></Statistic>
-                <Statistic value={filterPop(1000000)} title="1000k+"></Statistic>
-                <Statistic value={filterPop(500000)} title="500k+"></Statistic>
-                <Statistic value={filterPop(100000)} title="100k+"></Statistic>
-            </Space>
-            <div id="map" style={{ height: '60vh', width: '95vw' }} ></div>
+                <Space direction="horizontal" >
+                    <Statistic value={pop} title="Population"></Statistic>
+                    <Statistic value={"length" in props.data ? props.data.length : 0} title="Areas" ></Statistic>
+                    <Statistic value={filterPop(1000000)} title="1000k+"></Statistic>
+                    <Statistic value={filterPop(500000)} title="500k+"></Statistic>
+                    <Statistic value={filterPop(100000)} title="100k+"></Statistic>
+                </Space>
+                <div id="map" style={{ height: '60vh', width: '95vw' }} ></div>
             </Card>
             <Card title={"Magnetic total intensity at each city using WMM"}>
-            <div id="vishome" style={{ height: '60vh', width: '95vw' }}></div>
+                <div id="vishome" style={{ height: '60vh', width: '95vw' }}></div>
             </Card>
             <Card title={"elevation Plot of the areas"}>
-            <div id="visele" style={{ height: '60vh', width: '95vw' }}></div>
+                <div id="visele" style={{ height: '60vh', width: '95vw' }}></div>
             </Card>
             <Card title={<>
-            <Select style={{width:500}} title={""}
-                placeholder={'Select Cities to filter'} allowClear showSearch
-                mode="tags"
-                value={selectedCities}
-                onChange={(v) => { setSelectedCities(v); }} 
+                <Select style={{ width: 500 }} title={""}
+                    placeholder={'Select Cities to filter'} allowClear showSearch
+                    mode="tags"
+                    value={selectedCities}
+                    onChange={(v) => { setSelectedCities(v); }}
                 >
-                {props.data.map((b, _) => {
-                    return <Select.Option key={b.name} value={b.name} >{b.name}</Select.Option>
-                })}
-            </Select>
-            <Button onClick={()=> setPop(1)} >Submit</Button>
+                    {props.data.map((b, _) => {
+                        return <Select.Option key={b.name} value={b.name} >{b.name}</Select.Option>
+                    })}
+                </Select>
+                <Button onClick={() => setPop(1)} >Submit</Button>
             </>}>
 
-                <Table dataSource={data.filter(a=> selectedCities.length === 0 || selectedCities.includes(a.name) )} columns={ data.length > 0?  Object.keys(data[0]).map((a,i)=>  ({'title': a, 'dataIndex':a, 'key': a}) ) : []}></Table>
+                <Table dataSource={data.filter(a => selectedCities.length === 0 || selectedCities.includes(a.name))}
+                    style={{width: '100vw'}}
+                    scroll={{x : 1800}}
+                    columns={data.length > 0 ? Object.keys(data[0]).map((a, i) =>
+                        ({ 'title': a, 'dataIndex': a, 'key': a , 'fixed' : i < 2 ? 'left' : false }))
+                        : []}></Table>
             </Card>
+
         </>
     )
 }
