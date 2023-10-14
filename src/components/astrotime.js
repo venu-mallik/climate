65d519afec9fd7d9d9926f0713493dbb3fd851de
vrworkers <venu.mallik@gmail.com>
2023-10-14 23:44:11 +0530

    Add export csv button

--- src/components/astrotime.js
+++ src/components/astrotime.js
@@ -4,17 +4,19 @@ import {
     , Body, AstroTime, NextGlobalSolarEclipse, SearchLunarEclipse, MoonPhase, SearchMoonNode
 } from 'astronomy-engine';
 
-import {  Select,  Table, Divider } from 'antd';
+import { Select, Table, Divider } from 'antd';
 import { sachin_odis } from './_datasets';
-import { GoodDaysBy_Day_thithi,
-VeryGoodDaysBy_Day_Stars, 
-BadDaysByDay_thithi , BadDaysBy_Day_Stars, DLNL_Day_Star
- } from './_panchangerules';
+import {
+    GoodDaysBy_Day_thithi,
+    VeryGoodDaysBy_Day_Stars,
+    BadDaysByDay_thithi, BadDaysBy_Day_Stars, DLNL_Day_Star
+} from './_panchangerules';
+import { CSVLink } from "react-csv";
 
 const isSSREnabled = () => typeof window === 'undefined';
 
 function getLahari(d) {
-    
+
     if (d instanceof Date) {
 
         let a = (16.90709 * d.year / 1000) - 0.757371 * (d.year / 1000) * (d.year / 1000) - 6.92416100010001000;
@@ -30,34 +32,39 @@ function getNodes(tim, lahiri) {
     let t = 1.018;
     const JD = new AstroTime(new Date(1900, 0, 1, 0, 0, 0));
     let d = (JD.date.getTime() - tim.date.getTime()) / (1000 * 60 * 60 * 24);
-    let rahu = (259.183 - 0.05295 * (d+1) + 0.002078 * Math.pow(t, 2) + 0.000002 * Math.pow(t, 3)) % 360;
-    rahu = (rahu - lahiri) % 360 
+    let rahu = (259.183 - 0.05295 * (d + 1) + 0.002078 * Math.pow(t, 2) + 0.000002 * Math.pow(t, 3)) % 360;
+    rahu = (rahu - lahiri) % 360
     let ketu = rahu > 180 ? (rahu + 180) % 360 : rahu + 180;
     return { Rahu: Number(Math.abs(rahu)).toFixed(2), Ketu: Number(Math.abs(ketu)).toFixed(2) }
 }
 const allBodies = [Body.Sun, Body.Moon, Body.Mercury, Body.Venus, Body.Earth, Body.Mars, Body.Jupiter, Body.Saturn, Body.Uranus, Body.Neptune, Body.Pluto];
-const BodyDistances = { [Body.Sun] : 0.00 , [Body.Mercury] : 0.462 , 
-    [Body.Venus] : 0.728, [Body.Earth]: 1.014, [Body.Moon]: 1.0159,
-[Body.Mars]: 1.644, [Body.Jupiter]: 4.96 , [Body.Saturn]: 9.80,
-[Body.Uranus]:  19.6, [Body.Neptune]: 29.886, [Body.Pluto]: 34.76}
+const BodyDistances = {
+    [Body.Sun]: 0.00, [Body.Mercury]: 0.462,
+    [Body.Venus]: 0.728, [Body.Earth]: 1.014, [Body.Moon]: 1.0159,
+    [Body.Mars]: 1.644, [Body.Jupiter]: 4.96, [Body.Saturn]: 9.80,
+    [Body.Uranus]: 19.6, [Body.Neptune]: 29.886, [Body.Pluto]: 34.76
+}
 
-const BodyAdjustedDistances = { [Body.Sun] : 0.00 , [Body.Mercury] : 2.5 , 
-    [Body.Venus] : 5, [Body.Earth]: 7.5, [Body.Moon]: 7.6,
-[Body.Mars]: 10, [Body.Jupiter]: 12.5 , [Body.Saturn]: 15,
-[Body.Uranus]:  19.6, [Body.Neptune]: 29.886, [Body.Pluto]: 34.76}
+const BodyAdjustedDistances = {
+    [Body.Sun]: 0.00, [Body.Mercury]: 2.5,
+    [Body.Venus]: 5, [Body.Earth]: 7.5, [Body.Moon]: 7.6,
+    [Body.Mars]: 10, [Body.Jupiter]: 12.5, [Body.Saturn]: 15,
+    [Body.Uranus]: 19.6, [Body.Neptune]: 29.886, [Body.Pluto]: 34.76
+}
 
 
-function computeStrength(record ){
+function computeStrength(record) {
     let day = parseInt(record['weekday']);
-    let ga = VeryGoodDaysBy_Day_Stars[day].includes(record['star'])? 1 : 0;
-    let gb = GoodDaysBy_Day_thithi[day].includes(record['thihti'])? 1 : 0;
+    let gdds = VeryGoodDaysBy_Day_Stars[day].includes(record['star']) ? 1 : 0;
+    let gddt = GoodDaysBy_Day_thithi[day].includes(record['thihti']) ? 1 : 0;
     let dlnl = DLNL_Day_Star[day].includes(record['star']) ? 1 : 0;
 
 
-    let ba = BadDaysByDay_thithi[day].includes(record['thithi']) ? 1 : 0;
-    let bb = BadDaysBy_Day_Stars[day].includes(record['star'])? 1 : 0;
+    let bddt = BadDaysByDay_thithi[day].includes(record['thithi']) ? 1 : 0;
+    let bdds = BadDaysBy_Day_Stars[day].includes(record['star']) ? 1 : 0;
 
-    return {"good" : ga + gb + dlnl , "bad": ba + bb};
+    return { dlnl: dlnl, gdds: gdds, gddt: gddt, bdds: bdds, bddt: bddt };
+
 }
 
 function getValue(obs, date, bodies) {
@@ -72,35 +79,35 @@ function getValue(obs, date, bodies) {
 
     })
     let th = record[Body.Moon] - record[Body.Sun];
-    record['thithi'] = th < 0 ? parseInt((th+360)/12+1) : parseInt(1 +(th/12));
-    record['star'] = parseInt(record[Body.Moon]/27)+1;
+    record['thithi'] = th < 0 ? parseInt((th + 360) / 12 + 1) : parseInt(1 + (th / 12));
+    record['star'] = parseInt(record[Body.Moon] / 27) + 1;
     record['weekday'] = time.date.getDay();
 
-    let st  = computeStrength(record);
+    let st = computeStrength(record);
     let nn = getNodes(time, lahiri);
     return { ...record, ...nn, ...st };
 }
 
-function doTransformer(t,flag){
-    switch (flag){
+function doTransformer(t, flag) {
+    switch (flag) {
         case 'd1':
-            return 1 + parseInt(Number(t)/30);
+            return 1 + parseInt(Number(t) / 30);
         case 'd9':
-            return 1 + parseInt(((Number(t)*60)/2400));
+            return 1 + parseInt(((Number(t) * 60) / 2400));
         case 'star':
-            return 1 + parseInt((Number(t)*60)/800);
+            return 1 + parseInt((Number(t) * 60) / 800);
         case 'exal':
             return t
         case "debil":
             return t
         case "vargottama":
-            return parseInt((Number(t)*60)/2400) == parseInt(Number(t)/30) ? 1 : 0;
+            return parseInt((Number(t) * 60) / 2400) == parseInt(Number(t) / 30) ? 1 : 0;
         default:
             return t
     }
 }
 
-function drawSky(ctx, row){
+function drawSky(ctx, row) {
 
     ctx.reset()
     ctx.beginPath();
@@ -108,27 +115,26 @@ function drawSky(ctx, row){
     ctx.stroke();
 
 
-    Object.entries(row).map(([k,v],i)=>{
-        if(bodies.includes(k)){
-        ctx.beginPath();
-        ctx.arc(100, 100, BodyAdjustedDistances[k]*2.5 , Number(v), Number(v)+0.25);
-        ctx.stroke();
+    Object.entries(row).map(([k, v], i) => {
+        if (bodies.includes(k)) {
+            ctx.beginPath();
+            ctx.arc(100, 100, BodyAdjustedDistances[k] * 2.5, Number(v), Number(v) + 0.25);
+            ctx.stroke();
         }
     })
 }
 
 
-const SkyCanvas = (props) =>
-{
-        const canvas = useRef();
-        useEffect(() => {
-          const context = canvas.current.getContext('2d');
-          drawSky(context, props.row);
-        });
-        return (
+const SkyCanvas = (props) => {
+    const canvas = useRef();
+    useEffect(() => {
+        const context = canvas.current.getContext('2d');
+        drawSky(context, props.row);
+    });
+    return (
 
-          <canvas ref={canvas} title={props?.row?.time} height={200} width={200} />
-        );
+        <canvas ref={canvas} title={props?.row?.time} height={200} width={200} />
+    );
 }
 
 
@@ -139,12 +145,12 @@ export function AstroScales(props) {
     const [years, setYears] = useState([]);
     const [times, setTimes] = useState([]);
     const [saveData, setSaveData] = useState([]);
-    const [bodies,setBodies] = useState([Body.Sun, Body.Moon]);
+    const [bodies, setBodies] = useState([Body.Sun, Body.Moon]);
     const [mode, setMode] = useState("Sachin_ODIs");
-    const modes = ['EarthQuake',"Browse","Sachin_ODIs"];
+    const modes = ['EarthQuake', "Browse", "Sachin_ODIs"];
 
     const [transform, setTransform] = useState("degree");
-    const transforms = ["degree", "d1", "d9", "star","exal", "debil", "vargottama"]
+    const transforms = ["degree", "d1", "d9", "star", "exal", "debil", "vargottama"]
 
     const earthQuakeTimes = {
         '1960-05-22': 9.5, '1964-03-24': 9.2, '2004-12-26': 9.1, '2011-03-11': 9.0, '1952-11-04': 9.0
@@ -172,7 +178,7 @@ export function AstroScales(props) {
             //console.log(obs)
             let r = getValue(obs, d.date, bodies);
             let k = { ...r, time: new AstroTime(d.date).toString() };
-            if(mode === modes[2]){
+            if (mode === modes[2]) {
                 k['score'] = d.score;
             }
             arr.push(k)
@@ -186,17 +192,17 @@ export function AstroScales(props) {
         if (mode === modes[0]) {
             Object.keys(earthQuakeTimes).map((b, _) => {
                 let x = new Date(b);
-                a.push({date:x})
+                a.push({ date: x })
             })
         }
-        else if(mode === modes[1]){
+        else if (mode === modes[1]) {
             setYears([]);
             setData([]);
         }
-        else if(mode === modes[2]){
-            sachin_odis.map((rec,_)=> {
+        else if (mode === modes[2]) {
+            sachin_odis.map((rec, _) => {
                 let x = new Date(rec['Start Date'])
-                a.push({date : x, score: rec.Runs});
+                a.push({ date: x, score: rec.Runs });
             })
         }
         setDates(a);
@@ -234,31 +240,31 @@ export function AstroScales(props) {
                     return <Select.Option key={b} value={b} >{b}</Select.Option>
                 })}
             </Select>
-            { mode === "Browse" &&
-            <Select style={{ width: 500 }} title={""}
-                placeholder={'Select Years'} allowClear showSearch
-                mode="tags"
-                value={years}
-                onChange={(v) => {  setYears(v); }}
-            >
-                {Array(2200).fill().map((b, i) => {
-                    return <Select.Option key={`${i}years`} value={2200 - i} >{2200 - i}</Select.Option>
-                })}
-            </Select> }
-            
-            { years.length > 0 && mode === "Browse" &&
-            <Select style={{ width: 500 }} title={""}
-                placeholder={'Select Dates in year'} allowClear showSearch
-                mode="tags"
-                value={dates.map(a => a.date)}
-                onChange={(v) => { setDates(v.map(a => ({date:a}))); }}
-            >
-                {times.map((b, _) => {
-                    return <Select.Option key={b.ut} value={b.ut} >{b.toString()}</Select.Option>
-                })}
-            </Select> }
+            {mode === "Browse" &&
+                <Select style={{ width: 500 }} title={""}
+                    placeholder={'Select Years'} allowClear showSearch
+                    mode="tags"
+                    value={years}
+                    onChange={(v) => { setYears(v); }}
+                >
+                    {Array(2200).fill().map((b, i) => {
+                        return <Select.Option key={`${i}years`} value={2200 - i} >{2200 - i}</Select.Option>
+                    })}
+                </Select>}
 
-            
+            {years.length > 0 && mode === "Browse" &&
+                <Select style={{ width: 500 }} title={""}
+                    placeholder={'Select Dates in year'} allowClear showSearch
+                    mode="tags"
+                    value={dates.map(a => a.date)}
+                    onChange={(v) => { setDates(v.map(a => ({ date: a }))); }}
+                >
+                    {times.map((b, _) => {
+                        return <Select.Option key={b.ut} value={b.ut} >{b.toString()}</Select.Option>
+                    })}
+                </Select>}
+
+
             <Divider></Divider>
             {/*
                 
@@ -266,14 +272,23 @@ export function AstroScales(props) {
                     return <SkyCanvas  row={rec}  key={"canvas"+id}></SkyCanvas>
                 })
             */}
-            <Table dataSource={data} style={{width: '100vw'}} scroll={{x : 1500}}
+            <CSVLink
+                filename={"TableContent.csv"}
+                data={data}
+                className="btn btn-primary"
+            >
+                Download csv
+            </CSVLink>
+            <Table dataSource={data} style={{ width: '100vw' }} scroll={{ x: 1500 }}
                 columns={data.length > 0 ? Object.keys(data[0]).map((a, i) =>
-                    ({ 'title': a, 'dataIndex': a, 'key': a , 'fixed' : a === "time" ? "right": false,
-                        "render":  function (t,r,i) {
-                            if(['time','thithi'].includes(a)) return t;
-                            return doTransformer(t, transform);
+                ({
+                    'title': a, 'dataIndex': a, 'key': a, 'fixed': a === "time" ? "right" : false,
+                    "render": function (t, r, i) {
+                        if (['time', 'thithi'].includes(a)) return t;
+                        return doTransformer(t, transform);
 
-                        } }))
+                    }
+                }))
                     : []}></Table>
         </>
     )
