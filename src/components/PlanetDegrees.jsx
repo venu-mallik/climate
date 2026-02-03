aa4ea6b03d1b4595cd01ed87574b866f7c8b9ef6
vrworkers <venu.mallik@gmail.com>
2026-02-03 22:21:34 +0530

    save to remote - cleanup done

--- src/components/PlanetDegrees.jsx
+++ src/components/PlanetDegrees.jsx
@@ -4,7 +4,16 @@ import { DownloadOutlined, LeftOutlined, RightOutlined } from '@ant-design/icons
 import { Body, AstroTime, Equator, Horizon, Observer } from 'astronomy-engine';
 import dayjs from 'dayjs';
 import { planets, generateTimeseriesData, exportToCSV, getLahari } from './PlanetUtils';
-import { Stars } from './_panchangerules';
+
+const Stars = {
+  1: 'Ashwini', 2: 'Bharani', 3: 'Krittika', 4: 'Rohini',
+  5: 'Mrigashirsha', 6: 'Ardra', 7: 'Punarvasu', 8: 'Pushya',
+  9: 'Ashlesha', 10: 'Magha', 11: 'Poorva_Phalguni', 12: 'Uttara_Phalguni',
+  13: 'Hasta', 14: 'Chitra', 15: 'Swati', 16: 'Vishakha', 17: 'Anuradha',
+  18: 'Jyeshtha', 19: 'Moola', 20: 'Purva_Ashadha', 21: 'Uttara_Ashadha',
+  22: 'Shravana', 23: 'Dhanishta', 24: 'Shatabhisha', 25: 'Purva_Bhadrapada',
+  26: 'Uttara_Bhadrapada', 27: 'Revati', 28: 'Abhijit'
+};
 
 const { Option } = Select;
 
